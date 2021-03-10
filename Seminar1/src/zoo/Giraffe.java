e0d4b0cd8f86d5c305d6c6482765ba280136c5b6
Rares David <david_rares@yahoo.com>
2021-03-10 11:03:37 +0200

    first commit

---
+++ Seminar1/src/zoo/Giraffe.java
@@ -0,0 +1,12 @@
+package zoo;
+
+public class Giraffe extends Animal {
+	public float neckLength;
+	
+	public Giraffe(String name, float weight, float neckLength) {
+		super(name, weight);
+		this.neckLength = neckLength;
+		
+	}
+
+}
